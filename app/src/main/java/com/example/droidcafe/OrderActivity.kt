10ac71f95d3720c1d59ea298870825e35fd98243
Ibnu Huygenz <ibnuwdodo@gmail.com>
2023-10-03 08:20:39 +0700

    add: spinner value

--- app/src/main/java/com/example/droidcafe/OrderActivity.kt
+++ app/src/main/java/com/example/droidcafe/OrderActivity.kt
@@ -2,15 +2,34 @@ package com.example.droidcafe
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
-class OrderActivity : AppCompatActivity() {
+class OrderActivity : AppCompatActivity(), OnItemSelectedListener {
+    var cities = arrayOf<String?>("Jakarta", "Surabaya", "Bandung", "Medan", "Semarang", "Makassar", "Yogyakarta", "Palembang", "Tangerang", "Depok")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        val spin = findViewById<Spinner>(R.id.city_spinner)
+        spin.onItemSelectedListener = this
+
+        val ad: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_spinner_item,
+            cities)
+
+        ad.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item)
+
+        spin.adapter = ad
     }
 
 
@@ -41,4 +60,15 @@ class OrderActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?,
+                                view: View, position: Int,
+                                id: Long) {
+        Toast.makeText(applicationContext,
+            cities[position],
+            Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 }
